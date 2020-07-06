48c79bbb5ef2a7058c770d52a0588402164a3c46
Gustavo Souza <gtssouza94@gmail.com>
2020-07-06 12:08:01 -0300

    Fragments criados

--- app/src/main/java/com/example/atmconsult/MainActivity.java
+++ app/src/main/java/com/example/atmconsult/MainActivity.java
@@ -38,8 +38,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_principal, R.id.nav_clientes, R.id.nav_contato, R.id.nav_servico, R.id.nav_sobre)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
