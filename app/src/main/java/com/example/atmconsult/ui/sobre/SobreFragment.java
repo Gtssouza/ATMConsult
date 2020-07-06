2d0ad72822cb5d7fd319f92191702302b03f7383
Gustavo Souza <gtssouza94@gmail.com>
2020-07-06 13:54:59 -0300

    Fragment Sobre criado

--- app/src/main/java/com/example/atmconsult/ui/sobre/SobreFragment.java
+++ app/src/main/java/com/example/atmconsult/ui/sobre/SobreFragment.java
@@ -10,6 +10,9 @@ import android.view.ViewGroup;
 
 import com.example.atmconsult.R;
 
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -23,7 +26,23 @@ public class SobreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sobre, container, false);
+        Element versao = new Element();
+        versao.setTitle("Versao 1.0");
+        return new AboutPage(getActivity())
+                .setImage(R.drawable.logo)
+                .setDescription("Integer ornare purus euismod pulvinar tempor. Mauris vel egestas orci.")
+
+                .addGroup("Entre em contato")
+                .addEmail("atendimento@atmconsultoria.com.br","Envie um email")
+
+                .addGroup("Redes Sociais")
+                .addFacebook("josesilva", "Facebook")
+                .addInstagram("joseSilva", "Instagram")
+                .addWebsite("www.google.com", "Google")
+
+                .addItem(versao)
+
+                .create();
+        //return inflater.inflate(R.layout.fragment_sobre, container, false);
     }
 }
